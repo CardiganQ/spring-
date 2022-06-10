b7fa429066419b726655301769622e05f8567a66
wangbin <wangb@bsfit.com.cn>
2022-06-10 14:25:30 +0800

    spring学习

---
+++ src/main/java/study1/dao/UserDao.java
@@ -0,0 +1,13 @@
+package study1.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangbin
+ * @Date: 2022/06/10/14:18
+ * @Description:
+ */
+public interface UserDao {
+
+    public void getuser();
+}
---
+++ src/main/java/study1/dao/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package study1.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangbin
+ * @Date: 2022/06/10/14:19
+ * @Description:
+ */
+public class UserDaoImpl implements UserDao{
+
+    @Override
+    public void getuser() {
+        System.out.println("获取用户数据");
+    }
+
+}
